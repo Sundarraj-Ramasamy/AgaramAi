b87acc2cd62a18ba51c3fb833e1cd36ad6cb8235
Sundarraj Ramasamy <51262706+Sundarraj-Ramasamy@users.noreply.github.com>
2024-02-15 13:47:10 +0530

    Update Contact.js
    including map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -49,6 +49,7 @@ const Contact = () => {
 
         <button type="submit">Send Message</button>
       </form>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15648.18473974916!2d77.511216!3d11.331335!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba9139ea7a42fa7%3A0xbda41c0f30165be!2sagaramai!5e0!3m2!1sen!2sin!4v1707984893991!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
   );
 };
