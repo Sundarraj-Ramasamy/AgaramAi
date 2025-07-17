20566d7065c21c298de29b5d9410bf3b400c4b95
Savin-1989 <98261434+Savin-1989@users.noreply.github.com>
2025-07-17 17:27:13 +0530

    Create Production build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression';
 
 export default defineConfig({
+  base: '/AgaramAi/',
   plugins: [react(), compression()],
 });
+
