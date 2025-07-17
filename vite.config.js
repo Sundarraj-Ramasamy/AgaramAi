0d62ebab10c77c083a52c5685ef325cd686594f9
Savin-1989 <98261434+Savin-1989@users.noreply.github.com>
2025-07-17 17:34:23 +0530

    Removing jekyll yml file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  base: '/AgaramAi/',
+  base: '/',
   plugins: [react(), compression()],
 });
 
