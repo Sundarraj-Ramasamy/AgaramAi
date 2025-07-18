b3e4f4ccbc970dd806396c5af384d8227e249792
Savin-1989 <98261434+Savin-1989@users.noreply.github.com>
2025-07-18 13:05:14 +0530

    adding vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import compression from 'vite-plugin-compression';
 
 export default defineConfig({
   base: '/', // Or adjust if deploying under a subpath
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react(), compression()],
 });
