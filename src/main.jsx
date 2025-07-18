8f9cec5bdb9cafc43afef98c46e730c4d404b8a7
Savin-1989 <98261434+Savin-1989@users.noreply.github.com>
2025-07-18 12:37:50 +0530

    Prod build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
-import './index.css';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+console.log('App is mounting...');
 // Create a root
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  base: '/',
+  base: '/', // Or adjust if deploying under a subpath
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react(), compression()],
 });
-
